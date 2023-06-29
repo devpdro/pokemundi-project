fad4705390cf838d5c25bef2d223b96e2b988f38
victor <victorpedr879@gmail.com>
2023-06-29 17:49:29 -0300

    feat: finished navigation bar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,20 +1,31 @@
-import styles from '@/styles/NavBar.module.scss'
+import styles from "@/styles/NavBar.module.scss";
 
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NavBar() {
   return (
     <>
-      <nav className={styles}>
-        <div className="bg-orange-500">
-          <Link href="/">Logo</Link>
+      <nav className={styles.navbar}>
+        <div className={styles.logo}>
+          <Image
+            src="/images/pokeball.png"
+            width="40"
+            height="40"
+            alt="PokeMundi"
+          />
+          <h1>PokeMundi</h1>
         </div>
-        <ul>
+        <ul className={styles.link_items}>
           <li>
-            <Link href="/">Home</Link>
+            <Link href="/">
+              Início
+            </Link>
           </li>
           <li>
-            <Link href="/">Sobre</Link>
+            <Link href="/">
+              Sobre
+            </Link>
           </li>
         </ul>
       </nav>
