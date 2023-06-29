28f9b09ee7009bb5a5bbf2600c7130dad5d8ead1
victor <victorpedr879@gmail.com>
2023-06-29 19:26:19 -0300

    feat: calling api

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,33 @@
-export default function Home() {
+export async function getStaticProps() {
+  const pokemons = 154;
+  const api = "https://pokeapi.co/api/v2/pokemon/";
+
+  const request = await fetch(`${api}/?limit=${pokemons}`);
+  const data = await request.json();
+
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
+}
+
+export default function Home({ pokemons }) {
   return (
     <>
-    
+      <div>
+        <h1>PokeMundi</h1>
+
+        <ul>
+          {pokemons.map((pokemon) => (
+            <li key={pokemon.id}>{pokemon.name}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
