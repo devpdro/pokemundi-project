c6dd3d80d9b62bf419d4858beaebf37d0192c35d
victor <victorpedr879@gmail.com>
2023-06-29 19:06:51 -0300

    style: better organizing

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 import styles from "@/styles/Footer.module.scss"
 
-export default function Home() {
+export default function Footer() {
   return (
     <>
       <footer className={styles.container}>
--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,30 @@
-import About from "@/components/About"
+import styles from "@/styles/About.module.scss";
 
-export default function Home() {
+import Image from "next/image";
+
+export default function About() {
   return (
     <>
-    <About/>
+      <main className={styles.container}>
+        <div className={styles.box_text}>
+          <h1>Sobre o Projeto</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim quae
+            hic placeat, incidunt neque, aspernatur expedita repellendus natus
+            ea veritatis laudantium, rem et harum unde. Minus facere laudantium
+            repellendus modi.
+          </p>
+        </div>
+
+        <div className={styles.box_img}>
+          <Image
+            src="/images/charizard.png"
+            width="400"
+            height="400"
+            alt="Pokémon Charizard"
+          />
+        </div>
+      </main>
     </>
   );
 }
