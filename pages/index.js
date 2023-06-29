d9ac3b241466915ec202c2e86d1b7e4d6aca8e81
victor <victorpedr879@gmail.com>
2023-06-29 19:46:26 -0300

    feat: finished component home style

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
+import styles from "@/styles/Home.module.scss";
+
+import Image from "next/image";
+import Link from "next/link";
+
 export async function getStaticProps() {
   const pokemons = 154;
   const api = "https://pokeapi.co/api/v2/pokemon/";
@@ -19,8 +24,16 @@ export async function getStaticProps() {
 export default function Home({ pokemons }) {
   return (
     <>
-      <div>
-        <h1>PokeMundi</h1>
+      <div className={styles.container}>
+        <div className={styles.box_title}>
+          <h1>PokeMundi</h1>
+          <Image
+            src="/images/pokeball.png"
+            width="40"
+            height="40"
+            alt="PokeMundi"
+          />
+        </div>
 
         <ul>
           {pokemons.map((pokemon) => (
