b0032930c885bf1ea49609fd3cfe73b3a95a2e17
victor <victorpedr879@gmail.com>
2023-06-29 23:32:04 -0300

    feat: dynamic classes

--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import { space } from "postcss/lib/list";
+
 export async function getStaticProps(context) {
   const id = context.params.pokemonId;
 
@@ -30,5 +33,25 @@ export async function getStaticPaths() {
 }
 
 export default function Pokemon({ pokemon }) {
-  return <p>{pokemon.name}</p>;
+  return (
+    <>
+      <div>
+        <h1>{pokemon.name}</h1>
+        <Image
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+          width="120"
+          height="120"
+          alt={pokemon.name}
+        ></Image>
+        <h3>Número: <span>#{pokemon.id} </span></h3>
+        <h3>Tipo:{ pokemon.types.map((item, index) => (
+            <span key={index} className={`${styles.type} ${styles[`type_${item.type.name}`]} `}>{item.type.name}</span>
+        )) } </h3>
+
+        <h4>Altura: {pokemon.height * 10} cm</h4>
+        <h4>Peso: {pokemon.weight / 10} kg</h4>
+
+      </div>
+    </>
+  );
 }
